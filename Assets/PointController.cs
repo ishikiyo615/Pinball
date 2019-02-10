c09ef7b90a586601aee72e7e1e7ee9bbb05a71e2
ishikiyo615 <ishikiyo615@gmail.com>
2019-02-10 17:57:35 +0900

    PinBall Project: First Commitment
    PinBall Game made by following Lesson5 Guidance of Tech Academy Unity Course

---
+++ Assets/PointController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour
+{
+
+    //ポイント
+    int points = 0;
+
+    //ポイントを表示するテキスト
+    GameObject pointText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のPointTextオブジェクトを取得
+        pointText = GameObject.Find("PointText");
+
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        //タグに応じたポイントを追加
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            points += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            points += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            points += 15;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            points += 25;
+        }
+
+        //pointTextに現在の点数を表示
+        pointText.GetComponent<Text>().text = points.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+}
